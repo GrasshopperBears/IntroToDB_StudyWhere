2e164dc8c03ff13da20aa52886b5bfd6397093b9
Changyeon Kim <doli1573@kaist.ac.kr>
2018-05-18 13:57:26 +0900

    로그인 수정
    20180518 13:57

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,24 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm, reviewForm, registerForm
+from app.models import *
+from sqlalchemy import *
 
 
 @app.route('/')
 def home():
-    return render_template('home.html', title = 'Exp')
+    return render_template('home.html', title='Exp')
 
 
-@app.route('/login',  methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+            form.userid.data, form.remember_me.data))
         return redirect("/")
-    return render_template('login.html', title = 'Sign In', form= form)
+    return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/review', methods=['GET', 'POST'])
 def review():
@@ -27,6 +30,7 @@ def review():
 
     return render_template('review.html', title='Review', form=review)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register = registerForm()
@@ -36,3 +40,8 @@ def register():
         return redirect("/")
 
     return render_template('register.html', title='Register', form=register)
+
+@app.route('/location', methods=['GET','POST'])
+def location():
+    location = Location.search_locations_by_category(0)
+    return render_template('locations.html', title='Locations', location = location)
